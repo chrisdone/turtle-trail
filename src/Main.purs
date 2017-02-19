fc3ce038d11eeaec6a347d3a2bade1ce302d8ba6
Chris Done <chrisdone@gmail.com>
2017-02-19 15:42:02 +0000

    Add deps, draw circle

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,16 @@
 module Main where
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Timer (setInterval, TIMER)
 import Control.Monad.Eff.Console (log, CONSOLE)
+import Control.Monad.Eff.Ref
 import Data.Maybe(Maybe(..))
 import Graphics.Canvas (CANVAS)
 import Graphics.Canvas as Canvas
 import Math as Math
 import Prelude
 
-main :: Eff (console :: CONSOLE, canvas :: CANVAS) Unit
+main :: Eff (console :: CONSOLE, canvas :: CANVAS, timer :: TIMER, ref :: REF) Unit
 main = do
   mcanvas <- Canvas.getCanvasElementById "canvas"
   case mcanvas of
@@ -18,9 +20,16 @@ main = do
       height <- Canvas.getCanvasWidth canvas
       log ("Width " <> show width <> ", height " <> show height)
       ctx <- Canvas.getContext2D canvas
-      Canvas.beginPath ctx
-      Canvas.arc ctx {x: 50.0, y: 50.0, r: 20.0, start: 0.0, end: Math.pi*2.0}
-      Canvas.arc ctx {x: 100.0, y: 50.0, r: 20.0, start: 0.0, end: Math.pi*2.0}
-      Canvas.closePath ctx
-      Canvas.fill ctx
+      ref <- newRef 0.0
+      setInterval
+        50
+        (do r <- readRef ref
+            Canvas.clearRect ctx { x: 0.0, y: 0.0, w: width, h: height }
+            Canvas.save ctx
+            Canvas.beginPath ctx
+            Canvas.arc ctx {x: 50.0, y: 50.0, r: 20.0 * Math.sin r, start: 0.0, end: Math.pi*2.0}
+            Canvas.closePath ctx
+            Canvas.fill ctx
+            Canvas.restore ctx
+            writeRef ref (if r < Math.pi then Math.min (r + 0.1) Math.pi else 0.0))
       pure unit
