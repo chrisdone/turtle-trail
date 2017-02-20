60cc661d098f3d01deb0065be034d363e5020e06
Chris Done <chrisdone@gmail.com>
2017-02-20 09:07:26 +0000

    Add stuff

---
+++ src/Control.Monad.Eff.Ref.js
@@ -0,0 +1,38 @@
+"use strict";
+
+exports.refEq = function(x) {
+  return function(y){
+    return x===y;
+  };
+};
+
+exports.newRef = function (val) {
+  return function () {
+    return { value: val };
+  };
+};
+
+exports.readRef = function (ref) {
+  return function () {
+    return ref.value;
+  };
+};
+
+exports["modifyRef'"] = function (ref) {
+  return function (f) {
+    return function () {
+      var t = f(ref.value);
+      ref.value = t.state;
+      return t.value;
+    };
+  };
+};
+
+exports.writeRef = function (ref) {
+  return function (val) {
+    return function () {
+      ref.value = val;
+      return {};
+    };
+  };
+};
--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,85 @@
 module Main where
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Timer (setInterval, TIMER)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Ref
-import Data.Maybe(Maybe(..))
+import Control.Monad.Eff.Timer (setInterval, TIMER)
+import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.Traversable
 import Graphics.Canvas (CANVAS)
 import Graphics.Canvas as Canvas
 import Math as Math
 import Prelude
 
-main :: Eff (console :: CONSOLE, canvas :: CANVAS, timer :: TIMER, ref :: REF) Unit
+-- NOTE: need a way for one signal of a sensor to reach an actor
+-- elsewhere in the organism.
+
+newtype Organism = Organism
+  { cells :: Array (Ref Cell)
+  }
+
+newtype Cell = Cell
+  { x :: Number
+  , y :: Number
+  , neighbors :: Array (Ref Cell)
+  , r :: Number
+  }
+
+newtype Input = Input
+  { timeTick :: Int
+  , age :: Int
+  , xvelocity :: Number
+  , yvelocity :: Number
+  , local_energy :: Number
+  , total_energy :: Number
+  , rotation :: Number
+  , light :: Array Number
+  , sound :: Number
+  , local_nonconnected_touch :: Boolean
+  , foreign_touch :: Number
+  }
+
+data Sensor
+  = Tick NumberCondition
+  | Age NumberCondition
+  | XVelocityBelow NumberCondition
+  | XVelocityAbove NumberCondition
+  | YVelocityBelow NumberCondition
+  | YVelocityAbove NumberCondition
+  | LocalEnergy NumberCondition
+  | Rotation NumberCondition
+  | Light ArrayCondition
+  | Sound ArrayCondition
+  | LocalTouch Boolean
+  | ForeignTouch Boolean
+  | And Sensor Sensor
+  | Or Sensor Sensor
+
+data ArrayCondition
+  = All NumberCondition
+  | None NumberCondition
+  | Conditions (Array NumberCondition)
+
+data NumberCondition
+  = NumberAbove Number
+  | NumberBelow Number
+  | NumberInRange Number Number
+
+data Action
+  = Articulate Number
+  | Propel Number
+  | Absorb
+  | Reproduce
+  | ShareEnergy Number
+  | MakeSound { strength :: Number, duration :: Int }
+  | MakeLight { strength :: Number, duration :: Int }
+  | Grow Number
+  | Disconnect
+  | Connect
+
+data Strength
+
 main = do
   mcanvas <- Canvas.getCanvasElementById "canvas"
   case mcanvas of
@@ -20,16 +89,16 @@ main = do
       height <- Canvas.getCanvasWidth canvas
       log ("Width " <> show width <> ", height " <> show height)
       ctx <- Canvas.getContext2D canvas
-      ref <- newRef 0.0
       setInterval
         50
-        (do r <- readRef ref
-            Canvas.clearRect ctx { x: 0.0, y: 0.0, w: width, h: height }
+        (do Canvas.clearRect ctx { x: 0.0, y: 0.0, w: width, h: height }
             Canvas.save ctx
-            Canvas.beginPath ctx
-            Canvas.arc ctx {x: 50.0, y: 50.0, r: 20.0 * Math.sin r, start: 0.0, end: Math.pi*2.0}
-            Canvas.closePath ctx
-            Canvas.fill ctx
             Canvas.restore ctx
-            writeRef ref (if r < Math.pi then Math.min (r + 0.1) Math.pi else 0.0))
+            pure unit)
       pure unit
+
+circle ctx pos r = do
+  Canvas.beginPath ctx
+  Canvas.arc ctx { x: pos.x, y: pos.y, r: r, start: 0.0, end: Math.pi * 2.0 }
+  Canvas.stroke ctx
+  Canvas.closePath ctx
