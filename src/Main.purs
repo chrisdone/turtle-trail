b11aeb441460aa22f6048c2e37c875798eecc2ef
Chris Done <chrisdone@gmail.com>
2017-02-26 21:12:13 +0000

    Balls

--- src/Main.purs
+++ src/Main.purs
@@ -12,73 +12,14 @@ import Graphics.Canvas as Canvas
 import Math as Math
 import Prelude
 
--- NOTE: need a way for one signal of a sensor to reach an actor
--- elsewhere in the organism.
-
-newtype Organism = Organism
-  { cells :: Array (Ref Cell)
+type Circle =
+  { x  :: Number -- ^ X position in pixels.
+  , y  :: Number -- ^ Y position in pixels.
+  , xv :: Number -- ^ X velocity in pixels per frame.
+  , yv :: Number -- ^ Y velocity in pixels per frame.
+  , r  :: Number -- ^ Radius in pixels.
+  , c  :: String -- ^ Color.
   }
-
-newtype Cell = Cell
-  { x :: Number
-  , y :: Number
-  , neighbors :: Array (Ref Cell)
-  , r :: Number
-  }
-
-newtype Input = Input
-  { timeTick :: Int
-  , age :: Int
-  , xvelocity :: Number
-  , yvelocity :: Number
-  , local_energy :: Number
-  , total_energy :: Number
-  , rotation :: Number
-  , light :: Array Number
-  , sound :: Number
-  , local_nonconnected_touch :: Boolean
-  , foreign_touch :: Number
-  }
-
-data Sensor
-  = Tick NumberCondition
-  | Age NumberCondition
-  | XVelocityBelow NumberCondition
-  | XVelocityAbove NumberCondition
-  | YVelocityBelow NumberCondition
-  | YVelocityAbove NumberCondition
-  | LocalEnergy NumberCondition
-  | Rotation NumberCondition
-  | Light ArrayCondition
-  | Sound ArrayCondition
-  | LocalTouch Boolean
-  | ForeignTouch Boolean
-  | And Sensor Sensor
-  | Or Sensor Sensor
-
-data ArrayCondition
-  = All NumberCondition
-  | None NumberCondition
-  | Conditions (Array NumberCondition)
-
-data NumberCondition
-  = NumberAbove Number
-  | NumberBelow Number
-  | NumberInRange Number Number
-
-data Action
-  = Articulate Number
-  | Propel Number
-  | Absorb
-  | Reproduce
-  | ShareEnergy Number
-  | MakeSound { strength :: Number, duration :: Int }
-  | MakeLight { strength :: Number, duration :: Int }
-  | Grow Number
-  | Disconnect
-  | Connect
-
-data Strength
 
 main = do
   mcanvas <- Canvas.getCanvasElementById "canvas"
@@ -87,18 +28,88 @@ main = do
     Just canvas -> do
       width <- Canvas.getCanvasWidth canvas
       height <- Canvas.getCanvasWidth canvas
-      log ("Width " <> show width <> ", height " <> show height)
+      log (show width <> " " <> show height)
       ctx <- Canvas.getContext2D canvas
+      ballRefs <-
+        traverse
+          (\x -> newRef x)
+          [
+            { i: 0, x: 20.0, y: 30.0, r: 5.0, xv: 2.0, yv: 0.0, c: "#ff90ff", mass: 1.0 }
+          , { i: 1, x: 80.0, y: 50.0, r: 5.0, xv: -5.0, yv: 0.0, c: "#55ff00", mass: 1.0 }
+
+          , { i: 2, x: 20.0, y: 50.0, r: 5.0, xv: 1.0, yv: 0.0, c: "#ff0000", mass: 1.0 }
+          , { i: 3, x: 50.0, y: 50.0, r: 5.0, xv: -4.0, yv: 0.075, c: "#00ffaa", mass: 1.0 }
+
+          , { i: 4, x: 20.0, y: 100.0, r: 5.0, xv: 5.0, yv: 0.0, c: "#0000ff", mass: 1.0 }
+          , { i: 5, x: 85.0, y: 90.0, r: 10.0, xv: -3.0, yv: 0.0, c: "#ff00ff", mass: 2.0 }
+
+          , { i: 6, x: 65.0, y: 70.0, r: 5.0, xv: 0.1, yv: 2.0, c: "#ffff00", mass: 1.0 }
+          , { i: 7, x: 30.0, y: 30.0, r: 5.0, xv: -0.05, yv: 2.5, c: "#00ffff", mass: 1.0 }
+          ]
       setInterval
-        50
+        25
         (do Canvas.clearRect ctx { x: 0.0, y: 0.0, w: width, h: height }
             Canvas.save ctx
+            balls <- traverse readRef ballRefs
+            traverse
+              (\ref -> do
+                ball <- readRef ref
+                let ball' =
+                      foldl
+                        (\a0 b ->
+                          let xdist = ((a0.x + a0.xv) - (b.x + b.xv))
+                              ydist = ((a0.y + a0.yv) - (b.y + b.yv))
+                              distSquared = xdist*xdist + ydist*ydist
+                          in if a0.i == b.i
+                                then a0
+                                else
+                                  let a =
+                                        a0
+                                        { xv = if a0.x+a0.xv < 0.0 || a0.x+a0.xv > width
+                                               then -a0.xv
+                                               else a0.xv
+                                        , yv = if a0.y+a0.yv < 0.0 || a0.y+a0.yv > height
+                                               then -a0.yv
+                                               else a0.yv
+                                        }
+                                  in
+                                  if distSquared <= (a.r + b.r) * (a.r + b.r)
+                                     then let xvelocity = b.xv - a.xv
+                                              yvelocity = b.yv - a.yv
+                                              dotProduct = xdist*xvelocity + ydist*yvelocity
+                                          in if dotProduct > 0.0
+                                                then let collisionScale = dotProduct / distSquared
+                                                         xcol = xdist * collisionScale
+                                                         ycol = ydist * collisionScale
+                                                         combinedMass = a.mass + b.mass
+                                                         colWeightA = 2.0 * b.mass / combinedMass
+                                                         colWeightB = 2.0 * a.mass / combinedMass
+                                                     in a
+                                                        { xv = a.xv + colWeightA * xcol
+                                                        , yv = a.yv + colWeightA * ycol
+                                                        }
+                                                else a
+                                     else a
+                          )
+                        ball
+                        balls
+                let ball'' = ball'
+                      { x = ball'.x + ball'.xv
+                      , y = ball'.y + ball'.yv
+                      -- , xv = ball'.xv * 0.99
+                      -- , yv = ball'.yv * 0.99
+                      }
+                writeRef ref ball''
+                circle ctx ball'')
+              ballRefs
             Canvas.restore ctx
             pure unit)
       pure unit
 
-circle ctx pos r = do
+circle ctx c = do
   Canvas.beginPath ctx
-  Canvas.arc ctx { x: pos.x, y: pos.y, r: r, start: 0.0, end: Math.pi * 2.0 }
+  Canvas.setFillStyle c.c ctx
+  Canvas.arc ctx { x: c.x, y: c.y, r: c.r, start: 0.0, end: Math.pi * 2.0 }
   Canvas.stroke ctx
+  Canvas.fill ctx
   Canvas.closePath ctx
